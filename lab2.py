2741e1e967c97ce45417e93c73534e16c4a2e7c3
AlexEternity <Alex_Eternity@mail.ru>
2016-11-03 19:30:12 +0700

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -9,7 +9,7 @@ for line in f.readlines():
 a=[' ',' ',' ']
 for i in sorted(ip):
 	if i.find(a[1])==-1:
-		a=re.split(r'(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}',i)
+		a=re.split(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}',i)
 		print(' ')
 		print('Podset',a[1],':')
 		print(i)
